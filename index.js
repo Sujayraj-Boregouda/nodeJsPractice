1b428da49a2feaec7dde4baa058924d302847c2f
Sujayraj Boregouda <sujayraj.gouda@gmail.com>
2024-07-14 12:11:27 +0530

    Route Setup

---
+++ index.js
@@ -0,0 +1,11 @@
+// const {generateRandomNumber, celciusToFahrenheit} = require('./utils');
+
+// console.log(`Random Number: ${generateRandomNumber()}`)
+
+// console.log(`Celcius to Farenhiet: ${celciusToFahrenheit(0)}`)
+
+// import getPosts, {getPostsLength} from "./postController.js";
+
+// console.log(`Posts Length: ${getPostsLength()}`)
+
+// console.log(getPosts());
